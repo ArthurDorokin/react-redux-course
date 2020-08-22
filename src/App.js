89707cb4ecb4f51372b362bb29e609d334437b6c
ArthurDorokhin <a.u.dorokhin@gmail.com>
2020-08-22 15:08:32 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container pt-3">
+      <div className="row">
+          <div className="col">
+              form
+          </div>
+      </div>
+        <div className="row">
+            <div className="col">
+                post
+            </div>
+            <div className="col">
+                fetched col
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
